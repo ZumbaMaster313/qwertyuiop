6c89989bc7ec4bf66c6e10b4266b6b6f3a1a8aa9
Sebastian Carbonero <32375483+ZumbaMaster313@users.noreply.github.com>
2019-06-30 10:16:25 -0700

    Update qwerty.py

--- qwertyuiop/qwerty.py
+++ qwertyuiop/qwerty.py
@@ -10,18 +10,22 @@ import urllib3
 
 webserver = Flask(__name__)
 
+sep = '/'
+final = ""
+myString = ""
+newString = ""
+link = "./templates/result.html"
+
+
 @webserver.route("/")
 def home():
     return render_template("index.html"), 200
 
 @webserver.route('/go', methods=['POST'])
 def go():
-    final = ""
-    newString = ""
     myString = request.form['ecid']
-    path = "./templates/result.html"
-    
-    try: 
+
+    try:
         try:
             r = requests.get(myString)
         except requests.ConnectionError:
@@ -32,42 +36,69 @@ def go():
         
         newTxt = txt.replace('%','')
 
-        sep = '/'
         ar = myString.split(sep, 3)
         newAr = [ar[0], ar[1], ar[2]]
         newString = sep.join(newAr)
 
-        final = newTxt.replace('"/', '"'+newString+'/')
-
-        soup = bs(final, 'html.parser')
-
-        url = soup.find_all('a')
-        urlList = []
-        for u in url:
-            tmp = u.get('href')
-            urlList.append(tmp)
-
-        emptyList = list(filter(None, urlList))
-        newList = list(set(emptyList))
-
-        while True:
-            try:
-                newList.remove(newString+'/')
-            except ValueError:
-                break
-
-        for i in newList:
-            final = final.replace(i, '/'+i)
-     
-        s = final.encode('utf-8', 'ignore')
-        with open(path, 'wb') as f:
-            f.write(s)
-            f.close
+        writeHtml(newString, newTxt)
         
         return render_template("result.html"), 200
 
     except Exception:
         return render_template("error.html", error="Hey mate, your supposed to input a url...", help="* https://idiot.com *"), 200
+
+@webserver.route('/go', defaults={'path': ''})
+@webserver.route('/go/<path:path>')
+def proxy(path):
+    r = requests.get(path)
+    txt = r.text
+        
+    newTxt = txt.replace('%','')
+
+    ar = path.split(sep, 3)
+    newAr = [ar[0], ar[1], ar[2]]
+    newString = sep.join(newAr)
+
+    writeHtml(newString, newTxt)
+
+    return render_template("result.html"), 200
+
+
+def writeHtml(nString, txt):
+    final = txt.replace('"/', '"'+nString+'/')
+
+    soup = bs(final, 'html.parser')
+
+    url = soup.find_all('a')
+    urlList = []
+    for u in url:
+        tmp = u.get('href')
+        urlList.append(tmp)
+
+    emptyList = list(filter(None, urlList))
+    newList = list(set(emptyList))
+        
+    while True:
+        try:
+            newList.remove(nString+'/')
+        except ValueError:
+            break
+
+    for i in newList:
+        final = final.replace(i, 'go/'+i)
     
+    n = 0
+    replaceString = '"go/'
+    while n <= 100:
+        replaceString += 'go/'
+        final = final.replace(replaceString, '"go/')
+        n += 1
+    
+
+    s = final.encode('utf-8', 'ignore')
+    with open(link, 'wb') as f:
+        f.write(s)
+        f.close
+
 if (__name__ == "__main__"):
     webserver.run(debug=True, port=5055, host='0.0.0.0')
